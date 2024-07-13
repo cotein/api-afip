1402c4ac4fbb48e71163e1f9052c6281e73536d2
Diego Barrueta <diego.barrueta@gmail.com>
2024-07-13 09:58:51 -0300

    wsfecred

---
+++ src/Afip/WS/WSFECRED.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Cotein\ApiAfip\Afip\WS;
+
+use Cotein\ApiAfip\Constantes;
+use Illuminate\Support\Facades\Log;
+use SoapClient;
+use Exception;
+
+class WSFECRED extends WebService
+{
+    const SERVICE = 'wsfecred';
+
+    public function __construct($environment = 'testing', $company_cuit, $company_id, $user_id)
+    {
+        parent::__construct(self::SERVICE, $environment, $company_cuit, $company_id, $user_id);
+
+        $this->afip_params = [
+            'token' => $this->token,
+            'sign' => $this->sign,
+            'cuitRepresentada' => $this->cuitRepresentada
+        ];
+
+        $this->connect();
+    }
+
+    public function connect(): void
+    {
+        try {
+            $wsdl = strtoupper(self::SERVICE) . '_' . $this->environment;
+            $ws = WS_CONST::getWSDL($wsdl);
+            $this->soapHttp = new SoapClient($ws);
+        } catch (Exception $e) {
+            Log::error("Error en try catch WSPUC" . $e->getMessage() . ' - ' . $e->getCode());
+            throw new Exception($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function consultarObligadoRecepción($cuitConsultada, $fechaEmision)
+    {
+
+        $this->afip_params['cuitConsultada'] = $cuitConsultada;
+        $this->afip_params['fechaEmision'] = $fechaEmision;
+
+        return $this->soapHttp->consultarObligadoRecepción($this->afip_params);
+    }
+
+    public function Dummy()
+    {
+        return $this->soapHttp->Dummy();
+    }
+}
